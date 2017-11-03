0e20c3f38795a4b6c3176d05998afb67b486d394
Leon Guo <>
2017-11-03 17:00:13 +0800

    add Project

---
+++ ios/LGReachability/LGReachability.h
@@ -0,0 +1,14 @@
+//
+//  LGReachability.h
+//  LGReachability
+//
+//  Created by Leon Guo on 2017/11/3.
+//  Copyright © 2017年 Leon Guo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <RCTBridgeModule.h>
+
+@interface LGReachability : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/LGReachability/LGReachability.m
@@ -0,0 +1,35 @@
+//
+//  LGReachability.m
+//  LGReachability
+//
+//  Created by Leon Guo on 2017/11/3.
+//  Copyright © 2017年 Leon Guo. All rights reserved.
+//
+
+#import "LGReachability.h"
+
+@implementation LGReachability
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(checkStatusForAddress:(NSString *)address callBack:(RCTResponseSenderBlock)callback)
+{
+    Reachability *reachability = [Reachability reachabilityWithHostName:address];
+    
+    int flag = -1;
+    switch([reachability currentReachabilityStatus]){
+            
+        case NotReachable:
+            flag = 0;
+            break;
+        case ReachableViaWiFi:
+            flag = 1;
+            break;
+        case ReachableViaWWAN:
+            flag =2;
+            break;
+    }
+    callback(@[[NSNull null],@(flag)]);
+}
+
+
+@end
